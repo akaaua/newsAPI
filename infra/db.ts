73ae78f0867b213166f2a4eff67eb33c1fd4c8c3
akaaua <akaaua@gmail.com>
2022-08-08 09:06:07 -0400

    Dockerizando o Projeto

--- infra/db.ts
+++ infra/db.ts
@@ -1,7 +1,7 @@
 import * as mongoose from 'mongoose';
 
 class Database{
-    private DB_URL = 'mongodb://localhost:27017/curso_APIs_REST'
+    private DB_URL = 'mongodb://link-db/db_portal'
 
     createConnection(){
         mongoose.connect(this.DB_URL);
